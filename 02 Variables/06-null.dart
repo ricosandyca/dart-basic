139f3065d763dfd184fc6ba03776d86ab92c886e
Rico Sandyca <ricosandyca.if@gmail.com>
2020-12-05 18:52:48 +0700

    Add unsigned null variable

--- 02 Variables/06-null.dart
+++ 02 Variables/06-null.dart
@@ -3,4 +3,10 @@ main(List<String> args) {
 
   print('${nothing}'); // null
   print('${nothing.runtimeType}'); // Null
+
+  // unassigned value of a variable will be read as null in the runtime
+  String name;
+  print(name); // null
+  print(name.runtimeType); // Null
+  print(name == null); // true
 }

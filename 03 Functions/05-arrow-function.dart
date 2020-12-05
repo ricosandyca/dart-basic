8c1c2c520afc069f984ec7dd857e473652e39937
Rico Sandyca <ricosandyca.if@gmail.com>
2020-12-05 18:47:52 +0700

    Add arrow function notes

--- 03 Functions/05-arrow-function.dart
+++ 03 Functions/05-arrow-function.dart
@@ -1,6 +1,7 @@
 /**
  * Arrow function is a simple return statement function
  * Arrow function must return value
+ * Arrow function has the same lexical scopes definition with ES6 (JavaScript)
  */
 main(List<String> args) {
   String name = getMyName();
